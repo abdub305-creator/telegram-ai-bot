3c06e6ebdb6b13d29bfa8681c1c63682c81aa3d1
abdub305-creator <abdub305@gmail.com>
2026-01-05 00:51:21 +0400

    Create styles.js

---
+++ styles.js
@@ -0,0 +1,14 @@
+module.exports = {
+  anime: {
+    prompt: "anime style, detailed illustration",
+    models: ["Anything Diffusion", "Counterfeit"]
+  },
+  realistic: {
+    prompt: "photorealistic, cinematic lighting",
+    models: ["Realistic Vision"]
+  },
+  fantasy: {
+    prompt: "fantasy art, magical lighting",
+    models: ["DreamShaper"]
+  }
+};
